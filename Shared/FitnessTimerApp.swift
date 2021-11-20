7875b8f602d4f2597081dc40ad20e6aada876109
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2021-11-19 20:54:29 -0500

    Initial Commit

---
+++ Shared/FitnessTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTimerApp.swift
+//  Shared
+//
+//  Created by AlecNipp on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
